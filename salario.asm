a103a8aaa965572964aa84e5c2ac8e6aff49c56a
Cristian Samaniego <chris.rubiano42@gmail.com>
2015-11-20 12:43:35 -0700

    finalizado salario.asm

--- salario.asm
+++ salario.asm
@@ -17,10 +17,8 @@
 section .data
 	msgsalario db "Salario: ", 0x0
 	msgimpuesto db "Impuesto: ", 0x0
-    msg db "mayor a 50",0x0
-    msg2 db "mayor a 25", 0x0
-    msg3 db "mayor a 10", 0x0
-    msg4 db "menor a 10", 0x0
+    msgsueldo db "Sueldo neto: ", 0x0
+    rayitas db "----------------------", 0x0
 
 section .text
     global _start
@@ -40,13 +38,13 @@ sigArg:
     mov EDX, 50000						;movemos 50000 para comparar con el salario
     cmp EDX,EAX                         ;comparamos lo que esta en EAX con nuestro numero mayor
     jle mayora50	                    ;si es mayor, sobreescribimos el registro
-    mov EDX, 25000
-    cmp EDX, EAX
-    jle mayora25
-    mov EDX, 10000
-    cmp EDX, EAX
-    jle mayora10
-    jg menora10    
+    mov EDX, 25000                      ;movemos 25000 para comparar
+    cmp EDX, EAX                        ;comparamos 
+    jle mayora25                        ;si es mayor, saltamos a la etiqueta
+    mov EDX, 10000                      ;movemos 10000 para comparar
+    cmp EDX, EAX                        ;comparamos
+    jle mayora10                        ;si es mayor o igual, saltamos a la etiqueta
+    jg menora10                         ;si es menor, saltamos a la otra etiqueta
     dec ECX                             ;le restamos 1 a ECX
     jmp sigArg                          ;saltamos a sigArg para obtener otro argumento
     
@@ -58,38 +56,129 @@ noMasArgs:
     call quit                           ;salida
 
 mayora50:
-	push eax
-	mov eax,msgsalario
-	call sprint
-	pop eax
-	call iprintLF
-	push eax
-	mov eax, msgimpuesto
-	call sprint
-	pop eax
-	mov edx, 30
-	mul edx
-	mov ebx, 100
-	div ebx
+	push eax                           ;salvamos el sueldo al stack
+	mov eax,msgsalario                 ;movemos el mensaje a imprimir
+	call sprint                        ;imprimimos la cadena
+	pop eax                            ;sacamos el sueldo del stack a eax
+	call iprintLF                      ;imprimimos el entero
 
-	call iprintLF
-	dec ecx
-	jmp sigArg
+    push ecx                           ;salvamos ebx en el stack
+	mov ecx,eax                        ;movemos el sueldo a ebx
+    push eax                           ;guardamos eax en el stack               
+	mov eax, msgimpuesto               ;movemos el mensaje a imprimir
+	call sprint                        ;imprime
+	pop eax                            ;sacamos del stack a eax el sueldo
+	mov edx, 35                        ;movemos el 30 para sacar el porcentaje
+	mul edx                            ;multiplicamos
+	mov ebx, 100                       ;movemos el 100 para dividir
+	div ebx                            ;dividimos
+    call iprintLF                      ;imprimimos el entero con line feed
+	push eax                           ;salvamos el valor de eax
+    mov eax, msgsueldo                 ;movemos el mensaje a eax
+    call sprint                        ;imprimimos 
+    pop eax                            ;sacamos el valor del stack y lo ponemos en eax
+    sub ecx,eax                        ;restamos el impuesto al sueldo para calcular el sueldo neto
+    mov eax,ecx                        ;movemos a eax para imprimir
+    call iprintLF                      ;imprimimos con line feed
+    mov eax, rayitas                   ;movemos las rayitas a eax
+    call sprintLF                      ;imprimimos con line feed
+
+    pop ecx                            ;devolvemos ecx del stack (el numero del argumento)
+    dec ecx                            ;restamos para pasar al siguiente argumento
+	jmp sigArg                         ;volvemos a iterar
 
 mayora25:
-	mov eax, msg2
-	call sprintLF
-	dec ecx
-	jmp sigArg
+	push eax                           ;salvamos el sueldo al stack
+    mov eax,msgsalario                 ;movemos el mensaje a imprimir
+    call sprint                        ;imprimimos la cadena
+    pop eax                            ;sacamos el sueldo del stack a eax
+    call iprintLF                      ;imprimimos el entero
+
+    push ecx                           ;salvamos ebx en el stack
+    mov ecx,eax                        ;movemos el sueldo a ebx
+    push eax                           ;guardamos eax en el stack               
+    mov eax, msgimpuesto               ;movemos el mensaje a imprimir
+    call sprint                        ;imprime
+    pop eax                            ;sacamos del stack a eax el sueldo
+    mov edx, 30                        ;movemos el 30 para sacar el porcentaje
+    mul edx                            ;multiplicamos
+    mov ebx, 100                       ;movemos el 100 para dividir
+    div ebx                            ;dividimos
+    call iprintLF                      ;imprimimos el entero con line feed
+    push eax                           ;salvamos el valor de eax
+    mov eax, msgsueldo                 ;movemos el mensaje a eax
+    call sprint                        ;imprimimos 
+    pop eax                            ;sacamos el valor del stack y lo ponemos en eax
+    sub ecx,eax                        ;restamos el impuesto al sueldo para calcular el sueldo neto
+    mov eax,ecx                        ;movemos a eax para imprimir
+    call iprintLF                      ;imprimimos con line feed
+    mov eax, rayitas                   ;movemos las rayitas a eax
+    call sprintLF                      ;imprimimos con line feed
+
+    pop ecx                            ;devolvemos ecx del stack (el numero del argumento)
+    dec ecx                            ;restamos para pasar al siguiente argumento
+    jmp sigArg                         ;volvemos a iterar
 
 mayora10:
-	mov eax, msg3
-	call sprintLF
-	dec ecx
-	jmp sigArg
+	push eax                           ;salvamos el sueldo al stack
+    mov eax,msgsalario                 ;movemos el mensaje a imprimir
+    call sprint                        ;imprimimos la cadena
+    pop eax                            ;sacamos el sueldo del stack a eax
+    call iprintLF                      ;imprimimos el entero
+
+    push ecx                           ;salvamos ebx en el stack
+    mov ecx,eax                        ;movemos el sueldo a ebx
+    push eax                           ;guardamos eax en el stack               
+    mov eax, msgimpuesto               ;movemos el mensaje a imprimir
+    call sprint                        ;imprime
+    pop eax                            ;sacamos del stack a eax el sueldo
+    mov edx, 25                        ;movemos el 30 para sacar el porcentaje
+    mul edx                            ;multiplicamos
+    mov ebx, 100                       ;movemos el 100 para dividir
+    div ebx                            ;dividimos
+    call iprintLF                      ;imprimimos el entero con line feed
+    push eax                           ;salvamos el valor de eax
+    mov eax, msgsueldo                 ;movemos el mensaje a eax
+    call sprint                        ;imprimimos 
+    pop eax                            ;sacamos el valor del stack y lo ponemos en eax
+    sub ecx,eax                        ;restamos el impuesto al sueldo para calcular el sueldo neto
+    mov eax,ecx                        ;movemos a eax para imprimir
+    call iprintLF                      ;imprimimos con line feed
+    mov eax, rayitas                   ;movemos las rayitas a eax
+    call sprintLF                      ;imprimimos con line feed
+
+    pop ecx                            ;devolvemos ecx del stack (el numero del argumento)
+    dec ecx                            ;restamos para pasar al siguiente argumento
+    jmp sigArg                         ;volvemos a iterar
 
 menora10:
-	mov eax, msg4
-	call sprintLF
-	dec ecx
-	jmp sigArg
+	push eax                           ;salvamos el sueldo al stack
+    mov eax,msgsalario                 ;movemos el mensaje a imprimir
+    call sprint                        ;imprimimos la cadena
+    pop eax                            ;sacamos el sueldo del stack a eax
+    call iprintLF                      ;imprimimos el entero
+
+    push ecx                           ;salvamos ebx en el stack
+    mov ecx,eax                        ;movemos el sueldo a ebx
+    push eax                           ;guardamos eax en el stack               
+    mov eax, msgimpuesto               ;movemos el mensaje a imprimir
+    call sprint                        ;imprime
+    pop eax                            ;sacamos del stack a eax el sueldo
+    mov edx, 0                        ;movemos el 30 para sacar el porcentaje
+    mul edx                            ;multiplicamos
+    mov ebx, 100                       ;movemos el 100 para dividir
+    div ebx                            ;dividimos
+    call iprintLF                      ;imprimimos el entero con line feed
+    push eax                           ;salvamos el valor de eax
+    mov eax, msgsueldo                 ;movemos el mensaje a eax
+    call sprint                        ;imprimimos 
+    pop eax                            ;sacamos el valor del stack y lo ponemos en eax
+    sub ecx,eax                        ;restamos el impuesto al sueldo para calcular el sueldo neto
+    mov eax,ecx                        ;movemos a eax para imprimir
+    call iprintLF                      ;imprimimos con line feed
+    mov eax, rayitas                   ;movemos las rayitas a eax
+    call sprintLF                      ;imprimimos con line feed
+
+    pop ecx                            ;devolvemos ecx del stack (el numero del argumento)
+    dec ecx                            ;restamos para pasar al siguiente argumento
+    jmp sigArg                         ;volvemos a iterar
